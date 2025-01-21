b3cbf16dd473ad6c02d3fde9fcd733b297230f2c
ALEJANDRO GARCÍA PÉREZ <alexysergio2012@gmail.com>
2025-01-21 20:27:13 +0100

    Primer boton perfecto (Corregido)

--- GestionContactos/GestionContactos/Form1.cs
+++ GestionContactos/GestionContactos/Form1.cs
@@ -11,21 +11,75 @@ namespace GestionContactos
         const int Tam = 10;
         // Declaramos y damos tamaño a los vectores.
         string[] vNombre = new string[Tam];
+        string[] vTelefono = new string[Tam];
 
-        void LeerVNombre(string[] v)
+        void LeerVNombreTelefono(string[] vNomb, string[] vTelef)
         {
             int i;
-            // Leemos todos los elementos del vector.
-            for (i = 0; i < Tam; i++)
+            bool parar = false;
+            for (i = 0; i < Tam && !parar; i++)
             {
-                v[i] = Interaction.InputBox("Introduzca tu nombre: ");
+                if (vNomb[i] == null)
+                {
+                    string aux;
+                    bool rep = false;
+                    MessageBox.Show("Introducir la persona: " + i);
+                    aux = Interaction.InputBox("Introduzca tu nombre: ");
+                    rep = ComprobarIguales(aux, vNomb);
+                    if (!rep)
+                    {
+                        vNomb[i] = aux;                        
+                        vTelef[i] = Interaction.InputBox("Introduzca telefono: ");
+                    }
+                    else
+                    {
+                        MessageBox.Show("Persona ya introducida.");
+                    }
+                    parar = true;
+                }
             }
+
+
+            /*int i = 0;
+            // Leemos todos los elementos del vector.
+            while (i < Tam)
+            {
+                string aux;
+                bool rep = false;
+                MessageBox.Show("Introducir la persona: " + i);
+                aux = Interaction.InputBox("Introduzca tu nombre: ");
+                rep = ComprobarIguales(aux, vNomb);
+                if (!rep)
+                {
+                    vNomb[i] = aux;
+                    i++;
+                    vTelef[i] = Interaction.InputBox("Introduzca telefono: ");
+                }
+                else
+                {
+                    MessageBox.Show("Persona ya introducida.");
+                }              
+            }   */                    
+        }
+
+        bool ComprobarIguales(string aux, string[] vNomb)
+        {
+            bool iguales = false;
+            for (int j = 0; j < Tam && !iguales; j++)
+            {
+                if (aux == vNomb[j])
+                {
+                    iguales = true;
+                }
+            }
+            return iguales;
         }
 
         private void btnAñadContac_Click(object sender, EventArgs e)
         {
-            //Llamamos a la funcion para añadir el nombre y que se guarde.
-            LeerVNombre(vNombre);
+            //Llamamos a la funcion para añadir el nombre
+            //y telefono y que se guarde.            
+            LeerVNombreTelefono(vNombre, vTelefono);        
         }
     }
 }
